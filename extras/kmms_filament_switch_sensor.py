415f3a77e4ad52c3519f504ad553ba054a81ab2e
Michal Dvořák <mikee2185@gmail.com>
2024-03-08 10:59:34 +0100

    better KmmsRunoutHelper

--- extras/kmms_filament_switch_sensor.py
+++ extras/kmms_filament_switch_sensor.py
@@ -2,16 +2,24 @@ import logging
 
 
 class KmmsRunoutHelper:
-    def __init__(self, helper):
-        self.name = helper.name
-        self.printer = helper.printer
+    def __init__(self, config):
+        self.name = config.get_name().split()[-1]
+        self.printer = config.get_printer()
         self.reactor = self.printer.get_reactor()
         self.gcode = self.printer.lookup_object('gcode')
+        self.printer.load_object(config, 'pause_resume')
 
         # Read config
-        self.runout_gcode, self.insert_gcode = helper.runout_gcode, helper.insert_gcode
-        self.pause_delay = helper.pause_delay  # Time to wait after pause
-        self.event_delay = helper.event_delay  # Time between generated events
+        self.runout_pause = bool(config.getboolean('pause_on_runout', True))
+        self.runout_gcode = self.insert_gcode = None
+        gcode_macro = self.printer.load_object(config, 'gcode_macro')
+        if config.get('runout_gcode', None) is not None:
+            self.runout_gcode = gcode_macro.load_template(config, 'runout_gcode')
+        if config.get('insert_gcode', None) is not None:
+            self.insert_gcode = gcode_macro.load_template(config, 'insert_gcode')
+        self.run_always = config.getboolean('run_always', False)
+        self.pause_delay = config.getfloat('pause_delay', .5, above=.0)  # Time to wait after pause
+        self.event_delay = config.getfloat('event_delay', 3., above=0.)  # Time between generated events
 
         # Internal state
         self.min_event_systime = self.reactor.NEVER
@@ -22,13 +30,13 @@ class KmmsRunoutHelper:
         self.printer.register_event_handler("klippy:ready", self._handle_ready)
 
         # We are going to replace previous runout_helper mux commands with ours
-        prev = self.gcode.mux_commands.get("QUERY_FILAMENT_SENSOR")
-        prev_key, prev_values = prev
-        prev_values[self.name] = self.cmd_QUERY_FILAMENT_SENSOR
+        _replace_mux_command(self.gcode,
+                             "QUERY_FILAMENT_SENSOR", "SENSOR", self.name,
+                             self.cmd_QUERY_FILAMENT_SENSOR, desc=self.cmd_QUERY_FILAMENT_SENSOR_help)
 
-        prev = self.gcode.mux_commands.get("SET_FILAMENT_SENSOR")
-        prev_key, prev_values = prev
-        prev_values[self.name] = self.cmd_SET_FILAMENT_SENSOR
+        _replace_mux_command(self.gcode,
+                             "SET_PAUSE_ON_RUNOUT", "SENSOR", self.name,
+                             self.cmd_SET_PAUSE_ON_RUNOUT, desc=self.cmd_SET_PAUSE_ON_RUNOUT_help)
 
     def _handle_ready(self):
         self.min_event_systime = self.reactor.monotonic() + 2.  # Time to wait before first events are processed
@@ -49,7 +57,9 @@ class KmmsRunoutHelper:
 
     def _exec_gcode(self, prefix, template):
         try:
-            self.gcode.run_script(prefix + template.render() + "\nM400")
+            gcode = prefix + (template.render() if template is not None else '')
+            if gcode:
+                self.gcode.run_script(gcode + "\nM400")
         except Exception:
             logging.exception("Error running filament_switch_sensor %s handler" % self.name)
         self.min_event_systime = self.reactor.monotonic() + self.event_delay
@@ -66,25 +76,19 @@ class KmmsRunoutHelper:
             # when the sensor is disabled
             return
 
-        # Fire event
-        if is_filament_present:
-            self.printer.send_event('kmms:filament_insert', eventtime, self.name)
-        else:
-            self.printer.send_event('kmms:filament_runout', eventtime, self.name)
-
         # Determine "printing" status
         idle_timeout = self.printer.lookup_object("idle_timeout")
         is_printing = idle_timeout.get_status(eventtime)["state"] == "Printing"
         # Perform filament action associated with status change (if any)
         if is_filament_present:
-            if not is_printing and self.insert_gcode is not None:
+            if self.run_always or not is_printing:
                 # insert detected
                 self.min_event_systime = self.reactor.NEVER
                 logging.info(
                     "Filament Sensor %s: insert event detected, Time %.2f" %
                     (self.name, eventtime))
                 self.reactor.register_callback(self._insert_event_handler)
-        elif is_printing and self.runout_gcode is not None:
+        elif self.run_always or is_printing:
             # runout detected
             self.min_event_systime = self.reactor.NEVER
             logging.info(
@@ -95,7 +99,10 @@ class KmmsRunoutHelper:
     def get_status(self, eventtime):
         return {
             "filament_detected": bool(self.filament_present),
-            "enabled": bool(self.sensor_enabled)}
+            "enabled": bool(self.sensor_enabled),
+            "runout_pause": bool(self.runout_pause)}
+
+    cmd_QUERY_FILAMENT_SENSOR_help = "Query the status of the Filament Sensor"
 
     def cmd_QUERY_FILAMENT_SENSOR(self, gcmd):
         if self.filament_present:
@@ -104,17 +111,39 @@ class KmmsRunoutHelper:
             msg = "Filament Sensor %s: filament not detected" % (self.name)
         gcmd.respond_info(msg)
 
+    cmd_SET_FILAMENT_SENSOR_help = "Sets the filament sensor on/off"
+
     def cmd_SET_FILAMENT_SENSOR(self, gcmd):
         self.sensor_enabled = gcmd.get_int("ENABLE", 1)
 
+    cmd_SET_PAUSE_ON_RUNOUT_help = "Sets the pause on runout on/off"
 
-def runout_helper_attach(obj):
-    obj.runout_helper = KmmsRunoutHelper(obj.runout_helper)
+    def cmd_SET_PAUSE_ON_RUNOUT(self, gcmd):
+        self.runout_pause = gcmd.get_int("ENABLE", 1)
+
+
+def _replace_mux_command(gcode, cmd, key, value, func, desc=None):
+    # Remove existing, if it exists
+    prev = gcode.mux_commands.get(cmd)
+    if prev:
+        prev_key, prev_values = prev
+        if prev_key == key:
+            del prev_values[value]
+
+    # Register new
+    gcode.register_mux_command(cmd, key, value, func, desc=desc)
+
+
+def runout_helper_attach(obj, config):
+    obj.runout_helper = KmmsRunoutHelper(config)
     obj.get_status = obj.runout_helper.get_status
     return obj
 
 
 def load_config_prefix(config):
+    config.get_printer().load_object(config, 'pause_resume')
+
     import extras.filament_switch_sensor
     obj = extras.filament_switch_sensor.SwitchSensor(config)
-    return runout_helper_attach(obj)
+
+    return runout_helper_attach(obj, config)
