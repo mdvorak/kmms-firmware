05da171f92222fe909eb5b4914abd57c76125ca0
Michal Dvořák <mikee2185@gmail.com>
2024-03-17 13:58:28 +0100

    test pos 0

--- extras/kmms.py
+++ extras/kmms.py
@@ -220,6 +220,7 @@ class Kmms:
             self.respond_info("start_time = %.3f" % start_time)
             move_completion = self.endstop.start(start_time, endstop_names)
             self.respond_info("get_extruder_stepper_position")
+            self.set_position(0.)
             initial_pos = get_extruder_stepper_position(drive_extruder.get_object().extruder_stepper)
             self.respond_info("initial_pos=%.3f" % initial_pos)
 
@@ -234,11 +235,7 @@ class Kmms:
                 return self.reactor.NEVER
 
             self.reactor.register_timer(test, eventtime + 1)
-            self.toolhead.drip_move(self.relative_pos(100), self.max_velocity, move_completion)  # TODO pos
-            self.toolhead.drip_move(self.relative_pos(100), self.max_velocity, move_completion)  # TODO pos
-            self.toolhead.drip_move(self.relative_pos(100), self.max_velocity, move_completion)  # TODO pos
-            self.toolhead.drip_move(self.relative_pos(100), self.max_velocity, move_completion)  # TODO pos
-            self.toolhead.drip_move(self.relative_pos(100), self.max_velocity, move_completion)  # TODO pos
+            self.toolhead.drip_move(self.relative_pos(500), self.max_velocity, move_completion)  # TODO pos
 
             # Wait for move to finish
             self.respond_info("wait")
